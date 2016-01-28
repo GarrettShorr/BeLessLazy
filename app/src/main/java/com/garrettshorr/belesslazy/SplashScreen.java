715e45e037f92551c728f7a85c634851ffceb2a8
garrettshorr <g@garrettshorr.com>
2016-01-28 14:53:52 -0800

    Added a Splash Screen

---
+++ app/src/main/java/com/garrettshorr/belesslazy/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.garrettshorr.belesslazy;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by gshorr on 1/28/16.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
